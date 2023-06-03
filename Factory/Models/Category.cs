cdc2ce7c276bb53536fe7587ecc8666fc9db549a
Amanda Guan <ag.amandaguan@gmail.com>
2023-06-02 17:19:58 -0700

    add category

---
+++ Factory/Models/Category.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+    public class Category
+    {
+        private static List<Category> _instances = new List<Category> { };
+        public string Name { get; set; }
+        public int Id { get; }
+        public List<Machine> Machines { get; set; }
+
+        public Category(string categoryName)
+        {
+            Name = categoryName;
+            _instances.Add(this);
+            Id = _instances.Count;
+            Machines = new List<Machine> { };
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+
+        public static List<Category> GetAll()
+        {
+            return _instances;
+        }
+
+        public static Category Find(int searchId)
+        {
+            return _instances[searchId - 1];
+        }
+    }
+}
