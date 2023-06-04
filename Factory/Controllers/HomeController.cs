69e812fb36f8ae8009d678c5e67d0581ed3462ad
Amanda Guan <ag.amandaguan@gmail.com>
2023-06-04 01:51:23 -0700

    add splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly FactoryContext _db;
+
+        public HomeController(FactoryContext db)
+        {
+            _db = db;
+        }
 
         [HttpGet("/")]
         public ActionResult Index()
         {
-            return View();
+            Category[] cats = _db.Categories.ToArray();
+            Machine[] machines = _db.Machines.ToArray();
+            Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+            model.Add("categories", cats);
+            model.Add("machines", machines);
+            return View(model);
         }
-
     }
 }
