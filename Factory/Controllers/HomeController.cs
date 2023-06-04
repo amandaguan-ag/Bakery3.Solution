e29c74a5d94170cdb824783e89138b1177fd2e56
Amanda Guan <ag.amandaguan@gmail.com>
2023-06-04 02:18:25 -0700

    update splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -17,10 +17,10 @@ namespace Factory.Controllers
         [HttpGet("/")]
         public ActionResult Index()
         {
-            Category[] cats = _db.Categories.ToArray();
+            Engineer[] engineers = _db.Engineers.ToArray();
             Machine[] machines = _db.Machines.ToArray();
             Dictionary<string, object[]> model = new Dictionary<string, object[]>();
-            model.Add("categories", cats);
+            model.Add("engineers", engineers);
             model.Add("machines", machines);
             return View(model);
         }
